20954b158f6282eba33f34646da1737057d9b4f6
Lucas Rodrigues <lucasrodrigues062@gmail.com>
2023-07-28 17:04:07 -0300

    OMG, react router funcionando

---
+++ src/renderer/teste.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+export default function Teste() {
+  return (
+    <>
+      <h1>teste</h1>
+      <Link to="/">
+        <button type="button">Home</button>
+      </Link>
+    </>
+  );
+}
